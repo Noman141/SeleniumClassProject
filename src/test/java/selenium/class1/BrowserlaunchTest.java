01d7086cc7ae1c88a38d1b9501b2e451c5774d19
Noman141 <nomanjahid203@gmail.com>
2023-07-25 09:02:00 -0700

    first commit

---
+++ src/test/java/selenium/class1/BrowserlaunchTest.java
@@ -0,0 +1,19 @@
+package selenium.class1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserlaunchTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.facebook.com/");
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
